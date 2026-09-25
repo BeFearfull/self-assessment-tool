d6795507ee2149fa8f8879b33beaf9413b096829
BeFearfull <singh76aksanjay@gmail.com>
2026-09-25 14:13:15 +0530

    Added hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {HashRouter} from "react-router-dom";
 
+const redirect = sessionStorage.redirect;
+delete sessionStorage.redirect;
+
+if (redirect && redirect !== location.href) {
+  history.replaceState(null, '', redirect);
+}
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>,
 )
